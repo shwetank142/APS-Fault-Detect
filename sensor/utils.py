6bb15ab231eaa6b8613ecde96cd7729961b14193
shwetank142 <shwetankkumar196@gmail.com>
2023-02-26 17:16:38 +0530

    naya chij add hua

--- sensor/utils.py
+++ sensor/utils.py
@@ -3,7 +3,7 @@ from sensor.config import mongo_client
 from sensor.logger import logging
 from sensor.exception import SensorException
 import os,sys
-
+import yaml
 
 def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataFrame:
     
@@ -27,7 +27,14 @@ def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataF
         return df
     except Exception as e:
         raise SensorException(e, sys)
-
+def write_yaml_file(file_path,data:dict):
+    try:
+        file_dir = os.path.dirname(file_path)
+        os.makedirs(file_dir,exist_ok=True)
+        with open(file_path,"w") as file_writer:
+            yaml.dump(data,file_writer)
+    except Exception as e:
+        raise SensorException(e, sys)
 
 
 
